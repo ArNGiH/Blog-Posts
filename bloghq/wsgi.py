0765ffa314e3d733695d47195eebeab7dfc92f47
Aryan <aryannayar47@gmail.com>
2024-10-20 20:00:34 +0530

    Deployment 6

--- bloghq/wsgi.py
+++ bloghq/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bloghq.bloghq.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bloghq.settings')
 
 application = get_wsgi_application()
